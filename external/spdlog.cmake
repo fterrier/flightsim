ca852a99f614f1225692169c7ca5625a54e7ff8b
Francois Terrier <fterrier@gmail.com>
2018-06-21 10:02:38 +0100

    trying out hunter for dependencies

--- external/spdlog.cmake
+++
@@ -1,4 +0,0 @@
-message(STATUS "Installing spdlog via submodule")
-execute_process(COMMAND git submodule update --init -- external/spdlog
-  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-add_subdirectory(external/spdlog)
